11f50e3d43e91f813cc1fd9aa5d8d610230dfc98
m-bilu <02.mbilalkhan@gmail.com>
2023-09-06 20:19:12 -0400

    audioManip added and tested

---
+++ audioManip.py
@@ -0,0 +1,26 @@
+from pydub import AudioSegment
+import os
+import sys
+
+if __name__ == '__main__': 
+
+    inputName = sys.argv[1]
+    outputName = sys.argv[2]
+    targetRate = sys.argv[3]
+
+    for f in os.listdir(inputName):
+        if f.endswith('.wav'):
+            
+            #Load Audio
+            audio = AudioSegment.from_wav(os.path.join(input_folder, f))
+
+            # Set to target sample rate
+            audio = audio.set_frame_rate(targetRate)
+
+            # Output Path
+            outputPath = os.path.join(outputName, f)
+
+            # Export
+            audio.export(outputPath, format='wav')
+            print(f"Converted {f} to {targetRate} Hz and saved as {outputPath}")
+    print(f'Conversion complete, folder {outputName} is ready. ')
